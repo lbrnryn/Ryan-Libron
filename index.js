bc00133ece2e005b7fe96e37f054a545d9b87fcd
lbrnryn <lbrnryn@yahoo.com>
2023-04-11 01:19:29 +0800

    Portfolio's first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/css", express.static(path.join(__dirname, 'public')));
+app.set('json spaces', 2);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render("home");
+});
+
+const port = process.env.PORT || 2000;
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
